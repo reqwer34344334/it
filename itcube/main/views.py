841c2f8943da4f2f9df7c9ee01a432dcdbd7eda4
reqwer <qkwoqkwo228@gmail.com>
2023-04-20 10:19:05 +0900

    20.04

--- itcube/main/views.py
+++ itcube/main/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+
 def index(request):
     return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
